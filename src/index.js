872683a414a1e087d18919c55b85e8329f84f78a
adamjc <adamjc86@gmail.com>
2020-08-18 21:23:33 +0100

    Revert "Return correct response for APIGatewayV2"
    This reverts commit c17f388f1ddb5209f43e4fa952ee8dac7b4879a5.
    
    - Because we are using the 'quick create' APIV2, this automatically
    wraps the response/payload in that format, see: https://docs.aws.amazon.com/apigateway/latest/developerguide/http-api-develop-integrations-lambda.html

--- src/index.js
+++ src/index.js
@@ -1,13 +1,9 @@
+const fetch = require('node-fetch')
 const getLondonResidents = require('./get-london-residents.js')
 const getUsersInLondon = require('./get-users-in-london.js')
 const uniqBy = require('lodash.uniqby')
 
 exports.handler = async () => {
   const usersInLondon = await Promise.all([getLondonResidents(), getUsersInLondon()])
-  const dedupedUsers = uniqBy(usersInLondon.flat(Infinity), (el) => el.id)
-
-  return {
-    statusCode: 200,
-    body: JSON.stringify(dedupedUsers)
-  }
+  return uniqBy(usersInLondon.flat(Infinity), (el) => el.id)
 }
